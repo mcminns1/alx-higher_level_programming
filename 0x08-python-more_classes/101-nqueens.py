10379d4a1bbd7481b34c49dc5a3a5cefd03cb86f
Mcminn <mcminntheonly@gmail.com>
2023-09-05 10:14:32 -0700

    101up3

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,10 +54,10 @@ if __name__ == "__main__":
             clear_a(x)
             if reject(x, y):
                 a[x][1] = y
-                if (x == n - 1): #accepts the solution
+                if (x == n - 1): # accepts the solution
                     print(a)
                 else:
-                    nqueens(x + 1) #moves on to next x value to continue
+                    nqueens(x + 1) # moves on to next x value to continue
 
-    #start the recursive process at x = 0
+    # start the recursive process at x = 0
     nqueens(0)
